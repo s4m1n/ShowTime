7ec0fb84e3ddbff863f8a1ab8d4692db70e2a087
s4m1n <syan107405@gmail.com>
2021-02-20 13:14:53 +0600

    Inital commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default HomeScreen;
