07c660d3dd011c62856f978fa3a594f23d22c501
s4m1n <syan107405@gmail.com>
2021-03-05 10:34:53 +0600

    bug fixes

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,1 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyA6wR4IyBlatEhJrmbhcjyg6OxoaWyCoA4',
-  authDomain: 'netflix-clone-16309.firebaseapp.com',
-  projectId: 'netflix-clone-16309',
-  storageBucket: 'netflix-clone-16309.appspot.com',
-  messagingSenderId: '582344228764',
-  appId: '1:582344228764:web:f9915bce247334410f5259',
-};
+const firebaseConfig = REACT_APP_FIREBASE;
